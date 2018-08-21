2f52b2630febb7b38174a8432c988fa548253050
GLizaso <vectorize@hotmail.com>
2018-08-20 23:09:58 -0300

    ejemplo del workshop

---
+++ ejemploFunciones.py
@@ -0,0 +1,14 @@
+def fib2(n):
+    """Devuelve una lista conteniendo la serie de Fibonacci hasta n."""
+
+    resultado  # inicializo la lista completar!
+    a, b = 0, 1  # asignacion multiple
+    while a < n:
+        resultado.  # completar!
+        a, b = 0,1 # completar!
+    return  resultado# completar!
+
+
+# Invocamos la función definida.
+fib2000 = fib2(2000)
+type(fib2000)
